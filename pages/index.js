c02bfedc3d9ef1d3fff03f9433a38a51ba4989d3
BANGBANGROX <lakshya0809bang@gmail.com>
2022-09-30 14:43:47 +0530

    Added Footer

--- pages/index.js
+++ pages/index.js
@@ -40,9 +40,9 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         title2={"Everyone"}
         desc1={"Explore apartments, villas, Homes"}
         desc2={"and more"}
-        buttonText={"Explore Renting"}
-        linkName={"/search?purpose=for-rent"}
-        imageUrl={"https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"}
+        buttonText={"Explore Buying"}
+        linkName={"/search?purpose=for-sale"}
+        imageUrl={"https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"}
       />
       <Flex flexWrap={"wrap"}>
         {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
